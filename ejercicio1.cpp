feb94c33f6154bcaf9cfc9e020c2bebe526ac6bd
mcoronador <milagroscoronado16@gmail.com>
2026-06-19 22:50:53 -0500

    ejercicio1: agregue mensajes al ususario

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -3,14 +3,18 @@ using namespace std;
 
 int main() {
     int N;
+
+    cout << "Ingrese la cantidad de elementos: ";
     cin >> N;
 
     int a[1000];
 
+    cout << "Ingrese los " << N << " numeros: ";
     for (int i = 0; i < N; i++) {
         cin >> a[i];
     }
 
+    cout << "Secuencia en orden inverso: ";
     for (int i = N - 1; i >= 0; i--) {
         cout << a[i];
         if (i > 0)
