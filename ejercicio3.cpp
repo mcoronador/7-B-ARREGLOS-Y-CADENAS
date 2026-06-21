a331ee1a3e56ac790b2177cb5ccc52b640b38b65
mcoronador <milagroscoronado16@gmail.com>
2026-06-21 14:42:58 -0500

    ejercicio3: primer intento 0.1 vr

---
+++ ejercicio3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int P, n;
+    
+    while (cin >> P >> n) {
+        int frecuencia[101] = {0};
+
+        for (int i = 0; i < n; i++) {
+            int F;
+            cin >> F;
+            frecuencia[F]++;
+        }
+
+        }
+    }
+
+    return 0;
+}
