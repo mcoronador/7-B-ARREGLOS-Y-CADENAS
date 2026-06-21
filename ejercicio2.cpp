d8bf3501d1dd4709e46bd9985796c8ae094d4148
mcoronador <milagroscoronado16@gmail.com>
2026-06-21 14:33:10 -0500

    ejercicio2: mejora del programa

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -10,5 +10,21 @@ int main() {
         string palabra;
         cin >> palabra;
 
+        bool esPalindromo = true;
+        int n = palabra.size();
+
+        for (int i = 0; i < n / 2; i++) {
+            if (palabra[i] != palabra[n - 1 - i]) {
+                esPalindromo = false;
+                break;
+            }
+        }
+
+        if (esPalindromo)
+            cout << "P" << endl;
+        else
+            cout << "NP" << endl;
+    }
+
     return 0;
 }
