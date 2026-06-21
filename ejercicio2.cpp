57a17b275bd2d67761d11f1b4ebb5715967d9657
mcoronador <milagroscoronado16@gmail.com>
2026-06-21 14:26:44 -0500

    ejercicio2: primer intento

---
+++ ejercicio2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    while (N--) {
+        string palabra;
+        cin >> palabra;
+
+    return 0;
+}
