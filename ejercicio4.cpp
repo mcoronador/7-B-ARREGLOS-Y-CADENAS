7fb480f458d75b0f7a9c67556a170dbb8e438d11
mcoronador <milagroscoronado16@gmail.com>
2026-06-22 21:33:46 -0500

    tercer intento: implementacion de for

--- ejercicio4.cpp
+++ ejercicio4.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int main() {
     int N, C;
     cin >> N >> C;
 
-  
+    // Creamos un arreglo de 1000 vasos y los ponemos todos en 0
+    int vasos[1000] = {0}; 
+
+    // Ciclo para recibir cada una de las canicas
+    for (int i = 0; i < C; i++) {
+        int x;
+        cin >> x;    
+        vasos[x]++;  
+    }
+
+    // Ciclo para mostrar los resultados en la pantalla
+    for (int i = 1; i <= N; i++) {
+        cout << vasos[i] << endl; 
+    }
 
     return 0;
 }
